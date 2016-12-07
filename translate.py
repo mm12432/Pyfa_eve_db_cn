daf1d9a301149cc07e8f9f14a2903f7a894d9467
mao xiaoyong <453315655@qq.com>
2016-12-07 17:15:08 +0800

    framework done

--- translate.py
+++ translate.py
@@ -1,80 +1,72 @@
 # coding:utf-8
 
 import sqlite3
+import os
+
+list_table_column = [("invmarketgroups", "marketGroupName"),
+                     ("invgroups", "groupName"), ("invtypes", "typeName")]
 
 
-def load_market_groups():
-    file_market_groups = open('MarketGroups.txt', 'r')
-    lines_market_groups = file_market_groups.readlines()
-    file_market_groups.close()
-    return lines_market_groups
-
-
-def load_fitting_shiptypes():
-    file_fitting_shiptypes = open('FittingShipTypes.txt', 'r')
-    lines_fitting_shiptypes = file_fitting_shiptypes.readlines()
-    file_fitting_shiptypes.close()
-    return lines_fitting_shiptypes
-
-
-def load_items():
-    file_items = open('ItemsNonFiltered.txt', 'r')
-    lines_items = file_items.readlines()
-    file_items.close()
-    return lines_items
+def load_files_path(locate_path, list_files_path):
+    #abs_path = os.path.abspath(locate_path)
+    list_three_main = os.walk(locate_path)
+    for root, dirs, files in list_three_main:
+        for each in files:
+            if os.path.splitext(each)[1][1:] == "txt":
+                list_files_path.append(os.path.join(root, each))
 
 
 def get_pairs(line):
     return line.strip('\n\r').split(',')
 
 
-def translate_market_groups(cursor, lines_market_groups):
-    for i in range(len(lines_market_groups)):
-        each_pair = get_pairs(lines_market_groups[i])
-        sql = "update invmarketgroups set marketGroupName='{0}({1})' where marketGroupName='{0}'".format(
-            each_pair[0], each_pair[1])
-        cursor.execute(sql)
+def load_file(file_path):
+    file_to_read = open(file_path, 'r')
+    lines = file_to_read.readlines()
+    file_to_read.close()
+    if len(lines) > 2:
+        title_pair = get_pairs(lines[0])
+        if title_pair[0] == "flag" and title_pair[1] == "ok":
+            table_col = get_pairs(lines[1])
+            for table_name,column_name in list_table_column:
+                if table_col[0] == table_name and table_col[1] == column_name:
+                    return lines
+                
 
 
-def translate_fitting_shiptypes(cursor, lines_fitting_shiptypes):
-    for i in range(len(lines_fitting_shiptypes)):
-        each_pair = get_pairs(lines_fitting_shiptypes[i])
-        sql = "update invgroups set groupName='{0}({1})' where groupName='{0}'".format(
-            each_pair[0], each_pair[1])
-        cursor.execute(sql)
-
-
-def translate_items(cursor, lines_items):
-    for i in range(len(lines_items)):
-        each_pair = get_pairs(lines_items[i])
-        sql = "update invtypes set typeName='{0}({1})' where typeName='{0}'".format(
-            each_pair[0], each_pair[1])
-        cursor.execute(sql)
+def translate_lines(cursor, lines):
+    table_col = get_pairs(lines[1])
+    table_name = table_col[0]
+    column_name = table_col[1]
+    for i in range(2, len(lines)):
+        if lines[i][0:1] != "#":
+            each_pair = get_pairs(lines[i])
+            sql = "update {2} set {3}='{0}({1})' where {3}='{0}'".format(
+                each_pair[0], each_pair[1], table_name, column_name)
+            cursor.execute(sql)
 
 
 def main():
 
-    # 加载market groups
-    lines_market_groups = load_market_groups()
+    # 读取汉化文件路径
+    list_files_path = []
+    locate_path = 'locate'
+    load_files_path(locate_path, list_files_path)
 
-    # 加载fitting ship types
-    lines_fitting_shiptypes = load_fitting_shiptypes()
-
-    # 加载items
-    lines_items = load_items()
+    # 加载数据
+    list_lines = []
+    for each_file_path in list_files_path:
+        data = load_file(each_file_path)
+        if data is not None:
+            list_lines.append(data)
 
     # 连接数据库
     conn = sqlite3.connect('eve.db')
     cursor = conn.cursor()
 
     # 翻译数据库中的market group
-    translate_market_groups(cursor, lines_market_groups)
-
-    # 翻译数据库中的fitting ship type
-    translate_fitting_shiptypes(cursor, lines_fitting_shiptypes)
-
-    # 翻译数据库中的item
-    translate_items(cursor, lines_items)
+    for each_lines in list_lines:
+        translate_lines(cursor, each_lines)
 
     # 关闭数据库
     cursor.close()
